0420155214afb0ac6ffdc2a6590798e55d8696fb
parasdiwakar1 <paras208017@gmail.com>
2024-09-27 12:36:47 +0530

    user login and registetion

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import authRoutes from "./routes/authRoutes.js";
+import projectRoutes from './routes/projectRoutes.js';
+import testimonialRoutes from './routes/testimonialRoutes.js';
+import paymentRoutes from './routes/paymentRoutes.js';
+
+dotenv.config();
+
 const app = express();
 
-app.get('/', (req,res)=>{
-    res.send("Server is running.")
-})
+
+app.use(cors());
+app.use(express.json());
 
 
-app.listen(4000, ()=>{
-    console.log("Server is runnig on port 4000");
-    
-})
-console.log("ss")
-console.log("p")
+app.use('/api/auth', authRoutes); 
+app.use('/api/projects', projectRoutes); 
+app.use('/api/testimonials', testimonialRoutes); 
+app.use('/api/payments', paymentRoutes); 
+
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Atlas Cluster connected'))
+    .catch(err => console.log('Error: ' + err));
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
