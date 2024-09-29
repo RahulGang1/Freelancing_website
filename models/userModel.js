d54696fbcb1f48cd3bb1683d9e185530b3c0a6fd
parasdiwakar1 <paras208017@gmail.com>
2024-09-29 13:05:21 +0530

    m

--- models/userModel.js
+++ models/userModel.js
@@ -2,19 +2,12 @@ import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
 
 const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false }, 
+}, {
+    timestamps: true,
 });
 
 
@@ -24,14 +17,12 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
 
 
 userSchema.pre('save', async function (next) {
-    
     if (!this.isModified('password')) {
-        return next();
+        next();
     }
 
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next(); // Call next() after hashing the password
 });
 
 const User = mongoose.model('User', userSchema);
