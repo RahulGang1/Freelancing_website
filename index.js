be31b3dfd6af3e765ac6b42071c89ba28a7a8be8
RahulGang1 <rahul7055648508@gmail.com>
2024-10-09 11:11:43 +0530

    Add the payment routes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import projectSuggestions from './routes/ProjectSuggestionRoutes.js';
+import paymentRoutes from "./routes/PaymentRoute.js"
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,15 +13,14 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://paras22:BB2orhb1NHhtvT0J@cluster0.gejab.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect('mongodb+srv://paras22:BB2orhb1NHhtvT0J@cluster0.gejab.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error(err));
 
 // Routes
 app.use('/api/projects', projectSuggestions);
+app.use('/api', paymentRoutes);
+
 
 // Start server
 app.listen(PORT, () => {
